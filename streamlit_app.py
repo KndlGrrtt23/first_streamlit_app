d9d6fa0f63f676344e28574b3ff8b22357232752
Kendal Garrett <104144105+KndlGrrtt23@users.noreply.github.com>
2022-04-21 08:02:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ streamlit.title('My Mom\'s New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text(🥗'Kale, Spinach & Rocket Smoothie')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text(🐔'Hard-Boiled Free-Range Egg')
 streamlit.text(🥑🍞 'Avocado Toast')
 
