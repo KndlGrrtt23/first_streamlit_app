cb5f8a2e1a5d4b98aa387e5d2c1df3a84708eebf
Kendal Garrett <104144105+KndlGrrtt23@users.noreply.github.com>
2022-04-23 13:16:06 -0500

    Update streamlit_app.py
    Indexed names versus using numbers. Makes more sense!

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,8 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list - my_fruit_list.set_index('Fruit')
+
 # Adding a list here so they can pick the fruit they want
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
