8f2c3e73ecaf76d8ca82bae69a206dc1c426d1bd
Kendal Garrett <104144105+KndlGrrtt23@users.noreply.github.com>
2022-04-25 11:49:12 -0500

    Update streamlit_app.py
    Added  a Get Request & Fruitvice Advice

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import request
 
 streamlit.title('My Mom\'s New Healthy Diner')
 
@@ -21,3 +22,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #display the table on the page
 streamlit.dataframe(fruits_to_show)
+
+#New Section to Display FruityVice API Response
+fruityvice_repsonse = request.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
